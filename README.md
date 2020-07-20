d4dfdaff76608e04334eb72783d9d91d2c08cbfb
vany59 <levanyy2@gmail.com>
2020-07-20 22:43:26 +0700

    add readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# FAST FRONTEND DEVELOP
+
+## Install
+
+1. Clone project
+
+```
+git clone https://github.com/vany59/codebase-frontend.git
+```
+
+2. Install devpendence dependencies
+
+```
+npm i
+```
--- src/routers/index.js
+++ src/routers/index.js
@@ -2,24 +2,37 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import lazyLoader from '@utils/lazyloader'
 import routes from './router'
+import PageNotFound from '../pages/notfound'
 
 const AppRouters = () => {
+  const getRoute = routes
+    .map((router) => router.path)
+    .filter((router) => router === window.location.pathname)
   return (
     <>
       <Router>
         <Switch>
-          {routes.map((router) => {
-            const { path, exact, component } = router
-            const Mycomponent = lazyLoader(component)
-            return (
-              <Route
-                key={`${path}-${component}`}
-                path={path}
-                exact={exact}
-                component={Mycomponent}
-              />
-            )
-          })}
+          {getRoute.length &&
+            routes.map((router) => {
+              const { path, exact, component } = router
+              const Mycomponent = lazyLoader(component)
+              return (
+                <Route
+                  key={`${path}-${component}`}
+                  path={path}
+                  exact={exact}
+                  component={Mycomponent}
+                />
+              )
+            })}
+          {!getRoute.length && (
+            <Route
+              key="pageNotFound"
+              path={window.location.pathname}
+              exact
+              component={PageNotFound}
+            />
+          )}
         </Switch>
       </Router>
     </>
